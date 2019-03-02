dad9ac8d999b2bf4d5edc3c2e08ab9ce0afebdbe
Paul O'Shea <poshea48@msn.com>
2019-03-01 14:38:14 -1000

    refactored Profile

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,43 +1,38 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux';
-import ProfileHeader from "./ProfileHeader"
-import Spinner from '../common/Spinner'
-import { getProfileByUserId } from '../../actions/profileActions'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import ProfileHeader from "./ProfileHeader";
+import Spinner from "../common/Spinner";
+import { getProfileByUserId } from "../../actions/profileActions";
 
 class Profile extends Component {
   componentDidMount() {
-    this.props.getProfileByUserId(this.props.match.params.id)
+    this.props.getProfileByUserId(this.props.match.params.id);
   }
-  render () {
-    const { profile, loading } = this.props.profile
+  render() {
+    const { profile, loading } = this.props.profile;
     let profileContent;
     if (profile === null || loading) {
-      profileContent = <Spinner />
+      profileContent = <Spinner />;
     } else {
-      profileContent = (
-        <div>
-          {<ProfileHeader profile={profile}/>}
-        </div>
-      )
+      profileContent = <div>{<ProfileHeader profile={profile} />}</div>;
     }
-    return (
-      <div>
-        {profileContent}
-      </div>
-    )
+    return <div>{profileContent}</div>;
   }
 }
 
 Profile.propTypes = {
   profile: PropTypes.object.isRequired,
   getProfileByUserId: PropTypes.func.isRequired
-}
+};
 
 const mapStateToProps = state => {
   return {
     profile: state.profile
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, { getProfileByUserId })(Profile);
+export default connect(
+  mapStateToProps,
+  { getProfileByUserId }
+)(Profile);
