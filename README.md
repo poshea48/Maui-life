ab0726a051dbffd7a8b72742aa75623637b72c40
Paul O'Shea <poshea48@msn.com>
2019-07-18 11:17:50 -1000

    removed commented out code in TodosHome

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-This app was initially created for my friend who was coming to visit me in Maui HI.  The object was to have a platform to show him all that he did on the Island.
+This app was initially created for my friend who was coming to visit me in Maui HI. The object was to have a platform to show him all that he did on the Island.
 
 Functionality:
-  - Todos
-  - Hikes
-  - Posts/Comments/Likes/Unlikes
-  - Pictures (not completed)
-  - Location Map of areas visited (not completed)
 
-Wasnt able to finish for his visit, so I instead just made it into a Maui social media app.
+- Todos
+- Hikes
+- Posts/Comments/Likes/Unlikes
+- Pictures (not completed)
+- Location Map of areas visited (not completed)
+
+Wasn't able to finish for his visit, so I instead just made it into a Maui social media app.
 
 heroku addons:create mongolab
--- client/src/components/todos/TodosHome.js
+++ client/src/components/todos/TodosHome.js
@@ -23,6 +23,16 @@ const Container = styled.div`
   }
 `;
 
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: 720px) {
+    flex-direction: column;
+  }
+`;
+
 const Section = styled.div`
   width: 48%;
   @media (max-width: 720px) {
@@ -45,16 +55,6 @@ const FixedSection = styled(Section)`
 const Title = styled.h3`
   text-align: center;
   height: 1.2em;
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
-  @media (max-width: 720px) {
-    flex-direction: column;
-  }
 `;
 
 const Scroll = styled.div`
@@ -134,83 +134,6 @@ const TodosHome = ({ getTodos, toggleCompleted, deleteTodo }) => {
     </Container>
   );
 };
-
-// class TodosHome extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       todos: []
-//     };
-//   }
-//   componentDidMount() {
-//     this.props.getTodos();
-//   }
-//
-//   onChecked = todoId => () => {
-//     this.props.toggleCompleted(todoId);
-//   };
-//
-//   deleteTodo = todoId => () => {
-//     this.props.deleteTodo(todoId);
-//   };
-//
-//   render() {
-//     const { todos, loading } = this.props.todos;
-//     let todosContent;
-//
-//     if (todos === null || loading) {
-//       todosContent = <Spinner />;
-//     } else {
-//       // Check if logged in user has any todos
-//       if (isEmpty(todos)) {
-//         // User logged in with no todos
-//         todosContent = <p>You dont have anything to do yet</p>;
-//       } else {
-//         todosContent = (
-//           <div className="todos-content">
-//             <Scroll>
-//               <ul className="list-unstyled">
-//                 {todos.map((todo, i) => {
-//                   return (
-//                     <li className="todo-content" key={i}>
-//                       <TodoItem
-//                         todo={todo}
-//                         onChecked={this.onChecked}
-//                         deleteTodo={this.deleteTodo}
-//                       />
-//                     </li>
-//                   );
-//                 })}
-//               </ul>
-//             </Scroll>
-//           </div>
-//         );
-//       }
-//     }
-//
-//     return (
-//       <div className="home">
-//         <div className="container">
-//           <h3 className="text-center text-muted mb-4">Your Things to DO!</h3>
-//           <div className="row">
-//             <div className="col-md-6">{todosContent}</div>
-//             <div className="col-md-6">
-//               <AddTodo />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// TodosHome.propTypes = {
-//   getTodos: PropTypes.func.isRequired,
-//   toggleCompleted: PropTypes.func.isRequired,
-//   deleteTodo: PropTypes.func.isRequired,
-//   auth: PropTypes.object.isRequired,
-//   todos: PropTypes.object.isRequired
-// };
 
 const mapStateToProps = ({ todos, auth }) => ({
   todos,
